41068aebac12e4dd06e1107857bbfbdfb8ee5c8d
Uzair-Jamal <85803617+Uzair-Jamal@users.noreply.github.com>
2021-06-13 20:38:13 +0500

    Add files via upload

---
+++ Day2 task.py
@@ -0,0 +1,47 @@
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+Hours = "thirty"
+print(Hours)
+
+Days="Third days"
+print(Days[0])
+
+Challenge="I will win"
+print(Challenge[7:10])
+
+print(len(Challenge))
+
+print(Challenge.lower())
+
+x = "30 Days"
+y = "30 hours"
+c = x + y
+print(c)
+
+x = "30 Days"
+y = "30 hours"
+c = x + " " + y
+print(c)
+
+text = "Thirty days and Thirty hours"
+
+a = text.casefold()
+print(a)
+
+b = text.capitalize()
+print(b)
+
+d = text.find("h")
+print(d)
+
+e = text.isalpha()
+print(e)
+
+f = text.isalnum()
+print(f)
+
+
+
+
+
