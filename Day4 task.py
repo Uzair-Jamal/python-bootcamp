c972576b2fdd2cbdc839f4f7abd9fe18d9848ff6
Uzair-Jamal <85803617+Uzair-Jamal@users.noreply.github.com>
2021-06-15 20:29:33 +0500

    Add files via upload

---
+++ Day4 task.py
@@ -0,0 +1,27 @@
+print("Exercise questions")
+
+print("Task 1")
+
+a=b=c=50
+
+print("Divide",a,"by 10",a/10)
+
+print("Multiply",b,"by 50",b*50)
+
+print("Add",c,"by 60",c+60)
+
+#Question 2
+
+u="UZAIR"
+
+x = u.replace("A","G")
+print(x)
+
+#Question 3
+
+a=20
+b=20.22
+
+print("Converting b to integer",int(b))
+
+print("Converting a to float", float(a))
